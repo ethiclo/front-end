3473fc69316ba45fa4750d17d8ad42d717431981
ColePurboo <cole.purboo@icloud.com>
2023-03-18 18:51:40 -0400

    loaded in font-awesome

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,15 +5,43 @@ import Image from "next/image";
 
 function Login() {
     return (
-        <div className="flex bg-primary ">
+        <div>
+            <div className="flex">
             <div>
-                <Image height="400" width="500" src={logoImg}></Image>
+                <Image src={logoImg} width={700}
+                        height={250} />
             </div>
-            <div className="text-center">
-                <h1 className="text-white" >Welcome to Ethiclo!</h1>
+            <div>
+                <h3>Sign in here!</h3>
+                <div className="">
+                    <button className="">
+                            Login with Google
+                    </button>
+                </div>
             </div>
+            </div>
+            <Footer />
+
         </div>
+        
     )
 }
 
 export default Login
+
+
+export const Footer = () => {
+    return (
+        <div className="">
+            <div className="bg-gray-500 text-white">
+                <p className="">Cole Purboo, Rayce Ramsay, Michael Anthony-Dawes, Serah George</p>
+                <p>Copyright ETHICLO. All Rights Reserved</p>
+            </div>
+            <div className="">
+                <a href='https://www.linkedin.com/company/onyx-initiative/' target="_blank" rel="noreferrer">
+                    <i className="fa-user"> </i>
+                </a>
+            </div>
+        </div>
+    )
+}
