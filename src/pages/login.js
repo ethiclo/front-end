c2d2a60f4958fef421f4515dd18b7ab19be7a115
ColePurboo <cole.purboo@icloud.com>
2023-03-18 18:35:36 -0400

    started on login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,18 @@
 import React from "react";
+import logoImg from "../../public/img/ethiclo-logo.JPEG";
+import Image from "next/image";
 
 
 function Login() {
     return (
-        <h1>Welcome to Ethiclo!</h1>
+        <div className="flex bg-primary ">
+            <div>
+                <Image height="400" width="500" src={logoImg}></Image>
+            </div>
+            <div className="text-center">
+                <h1 className="text-white" >Welcome to Ethiclo!</h1>
+            </div>
+        </div>
     )
 }
 
