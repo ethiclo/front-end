7e4ddfdd362ccb6539b0792344827780f9a722ba
ColePurboo <cole.purboo@icloud.com>
2023-03-18 18:19:55 -0400

    completed ProductModal

--- src/sections/ProductModal.js
+++ src/sections/ProductModal.js
@@ -1,4 +1,4 @@
-import ProductComparisonCard from "@/components/ProductComparisonCard";
+import ProductComparisonCard, { ScoreComponent } from "@/components/ProductComparisonCard";
 import { dummyProducts } from "@/constants/dummy";
 import Image from "next/image";
 import React from "react";
@@ -22,7 +22,7 @@ export default function ProductModal(props) {
         onClick={(e) => e.stopPropagation()}
       >
         <div className="grid gap-2 grid-cols-[minmax(0,2fr),minmax(0,3fr)] h-full">
-          <div>
+          <div className="overflow-auto">
             <div className="h-0 pt-[150%] w-full relative">
               <Image
                 src={mainProduct.img_src}
@@ -30,6 +30,15 @@ export default function ProductModal(props) {
                 className="object-cover"
                 fill
               />
+            </div>
+            <div className="pl-5">
+            <h1 className="text-2xl text-black mb-2 pt-5 font-bold pb-1">{mainProduct.title}</h1>
+            <h1 className="text-xl ml-1  pb-2 text-gray-400">{mainProduct.brand} | {mainProduct.price}</h1>
+            <p className="ml-1 mb-4 ">{mainProduct.description}</p>
+            <div className="flex mb-4">
+                <p className="mr-3 font-bold">Sustainability Score:</p>
+                <ScoreComponent score={mainProduct.score} />
+            </div>
             </div>
           </div>
           <div className="flex-col h-full overflow-auto mb-2">
@@ -47,10 +56,10 @@ export default function ProductModal(props) {
         </div>
         <div>
           <button
-            className="absolute right-0 top-0 rounded-none bg-sky-500 hover:bg-sky-700"
+            className="font-bold h-8 w-8 rounded-full absolute bg-gray-300 text-white right-2 top-2 hover:opacity-70 transition-opacity"
             onClick={closeModal}
           >
-            Close
+            X
           </button>
         </div>
       </div>
