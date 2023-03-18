005e9165a8da66539777e32d153db0006151504c
Rayce Ramsay <ramsayrayce@gmail.com>
2023-03-18 16:20:54 -0400

    create basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
+import logoImg from "../../public/img/ethiclo-logo.JPEG";
 
 export default function Header({ ...props }) {
   return (
-    <header className="h-20 bg-yellow-300 border-b" {...props}>
-      <Link href="/">Ethiclo</Link>
+    <header
+      className="border-b-2 flex justify-between items-center pr-4"
+      {...props}
+    >
+      <Link href="/" className="hover:opacity-60 transition-opacity">
+        <Image src={logoImg} alt="Ethiclo logo" className="w-20" />
+      </Link>
+      <div className="rounded-full bg-primary w-10 h-10" />
     </header>
   );
 }
