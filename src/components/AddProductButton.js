95f11577086e031797a41e51c7baebc33d4bf91c
Rayce Ramsay <ramsayrayce@gmail.com>
2023-03-18 19:21:42 -0400

    add plus sign icon to add product button

--- src/components/AddProductButton.js
+++ src/components/AddProductButton.js
@@ -1,3 +1,6 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function AddProductButton({ ...props }) {
   return (
     <button
@@ -5,7 +8,7 @@ export default function AddProductButton({ ...props }) {
       className="bg-primary text-white font-bold h-10 w-10 rounded-full hover:opacity-70 transition-opacity"
       {...props}
     >
-      +
+      <FontAwesomeIcon icon={faPlus} />
     </button>
   );
 }
